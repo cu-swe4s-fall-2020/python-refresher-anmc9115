bd3978730a06b2b1c557f9cc069c272419041960
Anna McTigue <anmc9115@colorado.edu>
2020-09-11 02:01:46 +0000

    created get_column function

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,18 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    # opening file, addressing header
+    file = open(file_name, 'r')
+    header = None
+    # declares array where resuling values will be stored
+    results = []
+    for line in file:
+        if header == None:
+            line = header
+            continue
+        # Reading lines 
+        columns = line.rstrip.split(',')
+        # checking for value in each column of the line
+        for i in columns:
+            if query_value == query_column[i]:
+                results.append(columns[result_column])
+    return results
+ 
