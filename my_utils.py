ccf13ca03edcfa7f663f826e6d80b6586c1138ae
Anna McTigue <anmc9115@colorado.edu>
2020-09-11 02:45:35 +0000

    fixed finding query, added print cases

--- my_utils.py
+++ my_utils.py
@@ -1,18 +1,17 @@
 def get_column(file_name, query_column, query_value, result_column):
-    # opening file, addressing header
+    # open file
     file = open(file_name, 'r')
-    header = None
     # declares array where resuling values will be stored
     results = []
+    # skip first line
+    next(file)
+    
+    # reads file
     for line in file:
-        if header == None:
-            line = header
-            continue
-        # Reading lines 
-        columns = line.rstrip.split(',')
-        # checking for value in each column of the line
+        columns = line.rstrip().split(',')
+        #checking for value in each column of the line
         for i in columns:
-            if query_value == query_column[i]:
+            if query_value == columns[query_column]:
                 results.append(columns[result_column])
     return results
  
--- print_cases.py
+++ print_cases.py
@@ -1,6 +1,8 @@
+import my_utils as mu
+
 county='Boulder'
 county_column = 1
 cases_column = 4
 file_name = 'covid-19-data/us-counties.csv'
-cases = get_column()
+cases = mu.get_column(file_name,county_column,county,cases_column)
 print(cases)
