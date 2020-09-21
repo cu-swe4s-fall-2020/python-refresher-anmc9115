97fd358976a4f0736a7aee9e30d33a7f1b2b8ded
Anna McTigue <anmc9115@colorado.edu>
2020-09-21 00:41:04 +0000

    command line added, keword arg removed

--- my_utils.py
+++ my_utils.py
@@ -1,4 +1,4 @@
-def get_column(file_name, query_column, query_value, result_column=1):
+def get_column(file_name, query_column, query_value, results_column):
     # open file
     file = open(file_name, 'r')
     
@@ -14,7 +14,7 @@ def get_column(file_name, query_column, query_value, result_column=1):
         # adding each value from the result column which
         # has a value in query col that matches the query value
         if query_value == columns[query_column]:
-            results.append(columns[result_column])
+            results.append(columns[results_column])
     
     # close file
     file.close()
