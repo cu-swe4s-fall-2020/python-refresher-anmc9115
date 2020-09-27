2f1b635cc0d4ea7fc23df9c3e6271ad684fc2a34
Anna McTigue <anmc9115@colorado.edu>
2020-09-27 00:16:09 +0000

    add get_daily_count

--- my_utils.py
+++ my_utils.py
@@ -76,3 +76,70 @@ def get_column(file_name, query_column, query_value, results_column):
     results_array = array('i', results)
 
     return results_array
+
+
+def get_daily_count(file_name, query_column, query_value, results_column):
+    """Opens a csv and
+
+    Parameters
+    ----------
+    file_name: string
+            The path to the CSV file
+    query_column: integer
+            The column containing the query string
+    query_value: string
+            For each occurance of this string, values from the
+            results column of the same row will be collected
+    results_column: integer
+            The column containing values to be collected
+
+    Returns:
+    --------
+    daily_count: int array
+            values of specified column adjusted to daily
+            rather than cumulative values
+    """
+
+    # Runs get_column to recieve array value
+    results = get_column(file_name, query_column, query_value, results_column)
+    daily_count = array('i')
+
+    # Fills array with daily count
+    for i in range(len(results)):
+        if i == 0:
+            daily_count.append(results[i])
+        else:
+            daily_count.append(results[i] - results[i-1])
+    return daily_count
+
+
+def running_average(daily_count, window_size=5):
+    """Opens a csv and
+
+    Parameters
+    ----------
+    daily_cases: int array
+            Daily cases or deaths
+    window_size: int
+            Size of the window (range)
+
+    Returns:
+    --------
+    running_averages: float array
+            Array containing the running average
+            for the given window size
+    """
+    # open file, catch errors
+    try:
+        file = open(file_name, 'r')
+    except FileNotFoundError:
+        print("Could not find file: " + file_name)
+        sys.exit(1)
+    except PermissionError:
+        print("Could not access file: " + file_name)
+        sys.exit(2)
+
+    # skip first line
+    next(file)
+
+    return daily_numbers
