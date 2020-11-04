238d71b57881284f517456b13fcd511bbd6fdf86
Anna McTigue <anmc9115@colorado.edu>
2020-11-04 19:47:58 +0000

    add unit tests

--- test_hash_table.py
+++ test_hash_table.py
@@ -2,24 +2,33 @@
 """
 import hash_table
 import unittest
-import random
 
 class TestHashTable(unittest.TestCase):
     
-    def ascii_test_hash_function(self):
-        val = hash_table.hash_function('hello', 5)
-        self.assertEqual(val, 2)
+    def test_ascii_test_hash_function(self):
+        val = hash_table.ascii_hash_function('hello', 500)
+        hello_hash = (104+101+108+108+111) % 500
+        self.assertEqual(val, hello_hash)
     
     def test_put(self):
         htable = []
-        hash_table.put(htable, 'hello')
-        hash_table.put(htable, 'there')
-        hellohash = hash_table.hash_function('hello')
-        therehash = hash_table.hash_function('there')
-        print(htable)
+        for i in range(500):
+            htable.append([])
+        hash_table.put(htable, 500, 'testmystring', 8002334)
         
-#     def test_get(self):
-    
+        hash_val = hash_table.ascii_hash_function('testmystring', 500)
+        self.assertEqual(htable[hash_val][0][0], 'testmystring')
+        self.assertEqual(htable[hash_val][0][1], 8002334)
+
+    def test_get(self):
+        htable = [] 
+        for i in range(500):
+            htable.append([])
+        hash_table.put(htable, 500, 'testmystring', 8002334)
+        val = hash_table.get('testmystring', htable, 500)
+        val0 = hash_table.get('notmystring', htable, 500)
+        self.assertEqual(val, 8002334)
+        self.assertEqual(val0, None)
         
     
 if __name__ == '__main__':
